e1e1447b925562204272c6cb846a21195ad81c15
Patrick Altman <paltman@gmail.com>
2011-11-09 21:47:18 -0600

    Only accept POST requests

--- reminders/views.py
+++ reminders/views.py
@@ -1,8 +1,10 @@
 from django.conf import settings
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseNotAllowed
 
 
 def dismiss(request, label):
+    if request.method != "POST":
+        return HttpResponseNotAllowed(["POST"])
     if label not in settings.REMINDERS:
         return Http404()
     if not settings.REMINDERS.get(label).get("dismissable", True):
